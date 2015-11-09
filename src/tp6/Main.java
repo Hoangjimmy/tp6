b943500f685afdf25b01fc945c2f4f88fdd1fba7
Hoangjimmy <yumiquet@gmail.com>
2015-11-09 11:11:41 +0100

    SQLLoader issue

--- src/tp6/Main.java
+++ src/tp6/Main.java
@@ -8,36 +8,38 @@ import java.sql.Statement;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try(Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
-			// create a database connection
-			Statement statement = connection.createStatement();
-			statement.setQueryTimeout(30);  // set timeout to 30 sec.
-			statement.executeUpdate("drop table if exists WeatherInfo");
-			statement.executeUpdate("create table WeatherInfo (Name string Primary key, Temp double, TempMin double, TempMax double, humidity int)");
-			statement.executeUpdate("insert into WeatherInfo values(\"Paris\", 200,201,202,203)");
-			//statement.executeUpdate("insert into person values(2, 'yui')");
-			ResultSet rs = statement.executeQuery("select * from WeatherInfo");
-			while (rs.next()) {
-				// read the result set
-				System.out.println("name = " + rs.getString("Name"));
-				System.out.println("Temp = " + rs.getInt("Temp"));
-				System.out.println("TempMin = " + rs.getInt("TempMin"));
-				System.out.println("TempMax = " + rs.getInt("TempMax"));
-				System.out.println("Humidity = " + rs.getInt("Humidity"));
-			}
-		} catch (SQLException e) {
-			// if the error message is "out of memory", 
-			// it probably means no database file is found
-			System.err.println(e.getMessage());
-		}
-		IWeatherView view = new SwingWeatherView();
-		WeatherModel model = new WeatherModel();
-		SqliteWeatherDal loader = new SqliteWeatherDal();
-                WeatherInfo ll;
-                ll=loader.loadWeatherInfo("Paris");
-                System.out.println(ll.toString());
-		IWeatherController ctrl = new WeatherController(model, view, loader);
-		view.run();
-	}
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
+            // create a database connection
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            statement.executeUpdate("drop table if exists WeatherInfo");
+            statement.executeUpdate("create table WeatherInfo (Name string Primary key, Temp double, TempMin double, TempMax double, humidity int)");
+            statement.executeUpdate("insert into WeatherInfo values(\"Paris\", 200,201,202,203)");
+            //statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from WeatherInfo");
+            while (rs.next()) {
+                // read the result set
+                System.out.println("name = " + rs.getString("Name"));
+                System.out.println("Temp = " + rs.getInt("Temp"));
+                System.out.println("TempMin = " + rs.getInt("TempMin"));
+                System.out.println("TempMax = " + rs.getInt("TempMax"));
+                System.out.println("Humidity = " + rs.getInt("Humidity"));
+            }
+            IWeatherView view = new SwingWeatherView();
+            WeatherModel model = new WeatherModel();
+            SqliteWeatherDal loader = new SqliteWeatherDal();
+            WeatherInfo ll;
+            ll = loader.loadWeatherInfo("Paris");
+            System.out.println(ll.toString());
+            IWeatherController ctrl = new WeatherController(model, view, loader);
+            view.run();
+            connection.close();
+        } catch (SQLException e) {
+            // if the error message is "out of memory", 
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+
+    }
 }
--- src/tp6/SqliteWeatherDal.java
+++ src/tp6/SqliteWeatherDal.java
@@ -1,33 +1,29 @@
 package tp6;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 class SqliteWeatherDal implements IWeatherDal {
-	
+
+    private Connection connection;
     private Statement statement;
 
-    public SqliteWeatherDal() {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-
-        }
+    public SqliteWeatherDal() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+        statement = connection.createStatement();
+        statement.setQueryTimeout(30);
     }
 
     @Override
     public WeatherInfo loadWeatherInfo(String city) {
         WeatherInfo res = new WeatherInfo();
         try {
-            ResultSet rs = this.statement.executeQuery("SELECT Name FROM WeatherInfo Where Name = \" " + city + "\"");
+            ResultSet rs = this.statement.executeQuery("SELECT Name FROM WeatherInfo WHERE Name = \" " + city + "\";");
+            if(!rs.next())throw new RuntimeException() ;
+                   
             res.name = city;
             res.info.temp = rs.getDouble(2);
             res.info.minTemp = rs.getDouble(3);
@@ -40,9 +36,9 @@ class SqliteWeatherDal implements IWeatherDal {
 
         return res;
     }
-	
-	@Override
-	public void storeWeatherInfo(WeatherInfo info) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
+
+    @Override
+    public void storeWeatherInfo(WeatherInfo info) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
